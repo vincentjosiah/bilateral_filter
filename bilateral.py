f6fb1319f7a66d0f6564bd7e69fef130e3089053
Josiah Vincent <vincent.josiah@gmail.com>
2020-06-08 10:41:17 -0700

    end of project draft

--- bilateral.py
+++ bilateral.py
@@ -24,17 +24,14 @@ def sp_noise(image,prob):
                 output[i][j] = image[i][j]
     return output
 
-
-img = cv2.imread("image.jpg")
+img = cv2.imread("image2.jpg")
 original = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 a = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-a = sp_noise(a, 0.01)
-
-b = a.size
+# a = sp_noise(a, 0.05)
 
 a = a.astype('float64')
 
-mask_size = 7
+mask_size = 3
 cp_index = int(np.ceil(mask_size/2)) - 1     # 1
 
 variance_s = 50
@@ -51,8 +48,9 @@ for i in range(cp_index, a.shape[0]-cp_index):
         for k in range(i-cp_index, i-cp_index+mask_size):
             for l in range(j-cp_index, j-cp_index+mask_size):
                 x_i = np.array([k,l])
-                numerator += a[k,l] * gaussian(a[k, l], a[i, j], variance_r) * gaussian(x_i, x, variance_s)
-                denominator += gaussian(a[k, l], a[i, j], variance_r) * gaussian(x_i, x, variance_s)
+                if np.array_equal(x, x_i) == False:
+                    numerator += a[k,l] * gaussian(a[k, l], a[i, j], variance_r) * gaussian(x_i, x, variance_s)
+                    denominator += gaussian(a[k, l], a[i, j], variance_r) * gaussian(x_i, x, variance_s)
         filtered_img[i,j] = numerator/denominator
         numerator = 0
         denominator = 0
@@ -61,6 +59,7 @@ for i in range(cp_index, a.shape[0]-cp_index):
 
 filtered_img_uint = filtered_img.astype(np.uint8)
 
+cv2.imshow("original", original)
 cv2.imshow("filtered", filtered_img_uint)
 
 
